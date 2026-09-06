b741ba159bc0c1e4734e15c99670008a3e42a67a
Pedro Fonseca <pedrofonunes@gmail.com>
2026-09-06 15:07:00 -0300

    feat: first commit.

---
+++ src/test/java/com/pefonseca/spring/cloud/config/server/StartupAppTests.java
@@ -0,0 +1,13 @@
+package com.pefonseca.spring.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
